99b07dfb5523b395ffe7f6883879b71a51fb3bc6
phwork12@gmail.com <phwork12@gmail.com>
2023-01-15 18:02:59 +0900

    add subnetgroup

--- rds.tf
+++ rds.tf
@@ -20,7 +20,24 @@ resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
 # RDS option group
 # ---------------------------------------------
 resource "aws_db_option_group" "mysql_standalone_optiongroup" {
-  name                 = "${var.project}-${var.environment}-mysql-standalone-parametergroup"
+  name                 = "${var.project}-${var.environment}-mysql-standalone-optiongroup"
   engine_name          = "mysql"
   major_engine_version = "8.0"
+}
+
+# ---------------------------------------------
+# RDS subnet group
+# ---------------------------------------------
+resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
+  name = "${var.project}-${var.environment}-mysql-standalone-subnetgroup"
+  subnet_ids = [
+    aws_subnet.private_subnet_1a.id,
+    aws_subnet.private_subnet_1c.id
+  ]
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone-subnetgroup"
+    Project = var.project
+    Env     = var.environment
+  }
 }
